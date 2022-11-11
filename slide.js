bbb125b4cbbbb451cc3d56aee28b72876a13d16a
kingtanda <atandaoluwasijibomi@gmail.com>
2022-11-11 15:20:13 +0100

    third commit by Kingtanda, added media query

--- slide.js
+++ slide.js
@@ -1,10 +1,11 @@
 let slideIndex = 1 ;
 showSlides (slideIndex);
+
 function plusSlides(n) {
-    showSlides (slideIndex +=n);
+    showSlides (slideIndex += n);
 }
 function currentSlide(n) {
-    showSlides(slideIndex =n);
+    showSlides(slideIndex = n);
 }
 function showSlides(n) {
     let i;
@@ -16,9 +17,9 @@ function showSlides(n) {
     for (i = 0; i < slides .length; i++) 
     {slides[i] .style .display ="none";}
     for (i = 0; i < dots .length; i++)
-    {dots [i].className=dots[i].className.replace ("active", "");
+    {dots [i].className= dots [i].className.replace ("active");
 }
-slides[slideIndex-1].style.display ="block";
+slides [slideIndex-1].style.display ="block";
 dots [slideIndex-1] .className +="active";
 }
 let y = document.querySelector(".mySlides")
